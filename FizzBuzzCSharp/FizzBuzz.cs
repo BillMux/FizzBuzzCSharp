f634872c712d3bce98522d256d0cb4515b160a85
Bill Muxworthy <bill_muxworthy@hotmail.com>
2019-08-20 15:57:38 +0100

    First commit. Add references

---
+++ FizzBuzzCSharp/FizzBuzz.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FizzBuzzCSharp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ FizzBuzzCSharpTests/UnitTest1.cs
@@ -0,0 +1,19 @@
+//using FizzBuzzCSharp;
+using NUnit.Framework;
+
+namespace FizzBuzzTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
